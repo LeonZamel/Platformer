e4df3e4d4fb45e0f57775ad49796e6ce7b455791
Fresonty <leon.zamel@gmail.com>
2015-11-21 15:15:47 +0100

    Buttons with text
    Buttons can now have text

--- Platformer.py
+++ Platformer.py
@@ -17,7 +17,7 @@ GREEN = (0, 255, 0)
 
 
 def menu():
-    start_button = Button((100, 50), (100, 100), GREEN)
+    start_button = Button((100, 50), (500, 100), GREEN, "Start game")
     while True:
         # CHECK QUIT
         for event in pygame.event.get(QUIT):
@@ -38,11 +38,17 @@ def menu():
 
 
 class Button(object):
-    def __init__(self, pos, size, color):
+    def __init__(self, pos, size, color, text):
+        self.font_family = pygame.font.get_default_font()
+        self.font = pygame.font.Font(self.font_family, 20)
+        self.text = text
+        self.font_image = self.font.render(self.text, True, BLACK, )
+        self.font_rect = self.font_image.get_rect()
         self.size = size
+        self.center = (self.size[0] / 2 - self.font_rect.centerx, self.size[1] / 2 - self.font_rect.centery)
         self.clicks = None
         self.color_u = color
-        self.color_a = (222, 222, 0)
+        self.color_a = (0, 220, 0)
         self.color = self.color_u
         self.pos = pos
         self.rect = pygame.Rect(self.pos, self.size)
@@ -52,15 +58,14 @@ class Button(object):
     def check_click(self):
         if self.rect.collidepoint(pygame.mouse.get_pos()):
             self.color = self.color_a
-            self.image.fill(self.color)
             if pygame.mouse.get_pressed()[0]:
                 main.main()
         else:
             self.color = self.color_u
-            self.image.fill(self.color)
-
 
     def render(self):
+        self.image.fill(self.color)
+        self.image.blit(self.font_image, self.center)
         return self.image
 
 if __name__ == "__main__":
